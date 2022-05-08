ac7aab4590e3dbdd6fc345402bdc70d4781362c7
Len Smith <len@barrison.com>
2022-05-08 00:09:20 -0700

    Naive merging of classes

--- lib/mergewind.test.ts
+++ lib/mergewind.test.ts
@@ -2,9 +2,16 @@ import mergewind from "./mergewind";
 
 describe("mergewind", () => {
   it("merges two classes into one string", () => {
-    const defaults = "text-bold";
+    const defaults = "text-bold p-4";
     const overrides = "bg-red-100";
 
-    expect(mergewind(defaults, overrides)).toEqual("text-bold bg-red-100");
+    expect(mergewind(defaults, overrides)).toEqual("text-bold p-4 bg-red-100");
+  });
+
+  it("removes redundant classes", () => {
+    const defaults = "bg-red-500 p-4 m-4";
+    const overrides = "p-16 m-16";
+
+    expect(mergewind(defaults, overrides)).toEqual("bg-red-500 p-16 m-16");
   });
 });
--- lib/mergewind.ts
+++ lib/mergewind.ts
@@ -1,5 +1,28 @@
+function doesOverride(className: string, overrides: string[]): boolean {
+  if (overrides.length === 0) return false;
+  if (getNonSizeModifiedClass(overrides[0]) === className) return true;
+  return doesOverride(className, overrides.slice(1));
+}
+
+const getNonSizeModifiedClass = (className: string) => {
+  // HACK figure out the regex later
+  let parts = className.split("-");
+  parts.pop();
+  return parts.join(",");
+};
+
+const stripDefaults = (defaults: string[], overrides: string[]) => {
+  return defaults.filter((c) => {
+    return !doesOverride(getNonSizeModifiedClass(c), overrides);
+  });
+};
+
 const mergewind = (defaults: string, overrides: string) => {
-  return defaults + overrides;
+  const defaultClasses = defaults.split(" ");
+  const overrideClasses = overrides.split(" ");
+  const newDefaults = stripDefaults(defaultClasses, overrideClasses);
+
+  return [...newDefaults, ...overrideClasses].join(" ");
 };
 
 export default mergewind;
